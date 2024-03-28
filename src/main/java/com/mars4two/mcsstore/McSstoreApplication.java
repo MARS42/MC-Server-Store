8e0e1a7dee99a4e976ea577236d15ec668c521cd
Robert Mtz <msjred42@gmail.com>
2024-03-27 19:39:04 -0600

    Project setup

---
+++ src/main/java/com/mars4two/mcsstore/McSstoreApplication.java
@@ -0,0 +1,13 @@
+package com.mars4two.mcsstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McSstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McSstoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mars4two/mcsstore/McSstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mars4two.mcsstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McSstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
